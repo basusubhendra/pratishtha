ab789d9645e2e737f1beb4f7e9e6c72a1d2b28fa
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-22 11:42:06 +0000

    ocde

--- intfact.py
+++ intfact.py
@@ -4,7 +4,7 @@ import sys
 from decimal import Decimal
 from math import modf
 from zeros import zeros
-from mpmath import *
+from zeros_parts import parts
 
 def get_fractional_part(ctr):
     mp.prec=64
@@ -44,15 +44,17 @@ def characterize(num):
     ctr = 0
     zeros = []
     offsets = []
+    triplets = []
     while True:
         triplet = ""
         for i in range(0, 3):
              triplet = triplet + num[(ctr + i) % l]
+        triplets.append(triplet)
         mid_element = int(triplet[1])
         if mid_element == 0:
             mid_element = 10
         next_pair = triplet[0] + triplet[2]
-        zeros.append(get_zeros(next_pair, mid_element))
+        zeros.append([parts[int(triplet[0])][int(triplet[2])][:mid_element]])
         ctr = ctr + 1
         delta = ((l - 1) - (ctr + 1)) % 4
         npairs = delta/2.0
@@ -67,9 +69,9 @@ def characterize(num):
         offsets.append(npairs)
         if ctr + 1 == l - 1:
             break
-    return zeros,offsets
+    return triplets,zeros,offsets
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    zeros, offsets = characterize(num)
-    print(list(zip(zeros, offsets)))
+    triplets,zeros, offsets = characterize(num)
+    print(list(zip(triplets,zeros, offsets)))
--- t.py
+++ t.py
@@ -3,22 +3,25 @@ import sys
 f=open("./zeros6","r")
 g=open("./zeros_parts.py","w")
 lines = f.readlines()
+f.close()
 g.write("parts=")
-parts = []
+part = []
 for i in range(0, 10):
-    part = []
-    f.seek(0)
+    _parts_ = []
     for j in range(0, 10):
         print([i, j])
+        next_match = str(i) + str(j)
         ptr = 0
         ctr = 0
         count = 0
+        _part2_ = []
         while count < 10:
             zero = str(lines[ptr]).lstrip().rstrip()
             idx = zero.index(".")
             mantissa = zero[:idx]
             mantissa = mantissa[-2:]
-            if mantissa == str(i) + str(j):
+            if mantissa == next_match:
+                idx = zero.index(".")
                 zero_frac = zero[idx + 1:]
                 _part_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
                 zero_minus_1 = ""
@@ -32,10 +35,12 @@ for i in range(0, 10):
                 idx = zero_plus_1.index(".")
                 zero_frac = zero_plus_1[idx + 1:]
                 _part_1_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
-                part.append([_part_1_, _part_, _part_2_])
+                _part2_.append([_part_2_, _part_, _part_1_])
+                #input([zero[:idx], _part_2_, _part_, _part_1_])
                 ctr = ctr + 1
                 count = count + 1
             ptr = ptr + 1
-        parts.append(part)
-g.write(str(parts))
+        _parts_.append(_part2_)
+    part.append(_parts_)
+g.write(str(part))
 g.close()
