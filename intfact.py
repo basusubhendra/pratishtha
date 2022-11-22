af49c3b4f5d5f1ac442270257f6820b86b31bf16
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-22 05:18:17 +0000

    code

--- intfact.py
+++ intfact.py
@@ -3,45 +3,11 @@
 import sys
 from decimal import Decimal
 from math import modf
-from mpmath import *
-
-def get_fractional_part(ctr):
-    mp.prec=64
-    mp.dps=64
-    zero = str(zetazero(ctr).imag)
-    idx = zero.index(".")
-    mantissa = zero[:idx]
-    mantissa = mantissa[-2:]
-    zero = zero[idx+1:]
-    zero = zero[:8]
-    return mantissa, zero
-
-def _get_zero_(next_pair, ctr, ptr):
-        mantissa, zero = get_fractional_part(ctr)
-        if mantissa == next_pair:
-           return True, zero[ptr*2:ptr*2+2]
-        else:
-           return False, None
-
-def get_zeros(next_pair, mid_element):
-    mp.prec=64
-    mp.dps=64
-    ctr = 1
-    ptr = 0
-    _set_ = []
-    while ptr < mid_element:
-        success, zero = _get_zero_(next_pair, ctr, ptr % 4)
-        if success:
-            ptr = ptr + 1
-            success = False
-            _set_.append(zero)
-        ctr = ctr + 1
-    return _set_
+from zeros_parts import parts
 
 def characterize(num):
     l = len(num)
     ctr = 0
-    triplets = []
     zeros = []
     offsets = []
     while True:
@@ -52,22 +18,24 @@ def characterize(num):
         if mid_element == 0:
             mid_element = 10
         next_pair = triplet[0] + triplet[2]
-        triplets.append(triplet)
-        zeros.append(get_zeros(next_pair, mid_element))
+        zeros.append(parts[int(triplet[0])][int(triplet[2])][:mid_element])
         ctr = ctr + 1
-        delta = (l - 1) - (ctr + 1)
+        delta = ((l - 1) - (ctr + 1)) % 4
         npairs = delta/2.0
         dec = modf(Decimal(npairs))[0]
         if dec > 0:
-            npairs = [int(npairs), int(npairs) + 1]
+            if mid_element - int(npairs) == 0:
+                npairs = [0,9]
+            else:
+                npairs = [mid_element-int(npairs), mid_element-(int(npairs) + 1)]
         else:
-            npairs = [int(npairs)]
+            npairs = [mid_element-int(npairs)]
         offsets.append(npairs)
         if ctr + 1 == l - 1:
             break
-    return zeros,triplets, offsets
+    return zeros,offsets
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    zeros,triplets, offsets = characterize(num)
-    print(list(zip(triplets, zeros, offsets)))
+    zeros, offsets = characterize(num)
+    print(list(zip(zeros, offsets)))
