524456722d9705c94e7d0b02d0a1dd1bff049adf
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-22 10:44:20 +0000

    code

--- intfact.py
+++ intfact.py
@@ -3,7 +3,41 @@
 import sys
 from decimal import Decimal
 from math import modf
-from zeros_parts import parts
+from zeros import zeros
+from mpmath import *
+
+def get_fractional_part(ctr):
+    mp.prec=64
+    mp.dps=64
+    zero = str(zetazero(ctr).imag)
+    idx = zero.index(".")
+    mantissa = zero[:idx]
+    mantissa = mantissa[-2:]
+    zero = zero[idx + 1:]
+    zero = zero[:8]
+    return mantissa, zero
+
+def _get_zero_(next_pair, ctr, ptr):
+    mantissa, zero = get_fractional_part(ctr)
+    if mantissa == next_pair:
+        return True, zero[(ptr % 4)*2:(ptr % 4)*2 + 2]
+    else:
+        return False, None
+
+def get_zeros(next_pair, mid_element):
+    mp.prec=64
+    mp.dps=64
+    ctr = 1
+    ptr = 0
+    _set_ = []
+    while ptr < mid_element:
+        success, zero = _get_zero_(next_pair, ctr, ptr % 4)
+        if success:
+            ptr = ptr + 1
+            success = False
+            _set_.append([zero, ctr])
+        ctr = ctr + 1
+    return _set_
 
 def characterize(num):
     l = len(num)
@@ -18,7 +52,7 @@ def characterize(num):
         if mid_element == 0:
             mid_element = 10
         next_pair = triplet[0] + triplet[2]
-        zeros.append(parts[int(triplet[0])][int(triplet[2])][:mid_element])
+        zeros.append(get_zeros(next_pair, mid_element))
         ctr = ctr + 1
         delta = ((l - 1) - (ctr + 1)) % 4
         npairs = delta/2.0
--- t.py
+++ t.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python3
 import sys
 f=open("./zeros6","r")
-g=open("./zeros.py","w")
+g=open("./zeros_parts.py","w")
 lines = f.readlines()
-g.write("zeros=[")
-for line in lines:
-    l = line.rstrip().lstrip()
-    idx = l.index(".")
-    l = l[:idx]
-    l = l[-2:]
-    if line == lines[-1]:
-        g.write("\"" + str(l) + "\"" )
-    else:
-        g.write("\"" + str(l) + "\"" + ",")
-g.write("]")
+g.write("parts=")
+parts = []
+for i in range(0, 10):
+    part = []
+    f.seek(0)
+    for j in range(0, 10):
+        print([i, j])
+        ptr = 0
+        ctr = 0
+        count = 0
+        while count < 10:
+            zero = str(lines[ptr]).lstrip().rstrip()
+            idx = zero.index(".")
+            mantissa = zero[:idx]
+            mantissa = mantissa[-2:]
+            if mantissa == str(i) + str(j):
+                zero_frac = zero[idx + 1:]
+                _part_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
+                zero_minus_1 = ""
+                _part_2 = ""
+                if ptr > 0:
+                    zero_minus_1 = str(lines[ptr-1]).lstrip().rstrip()
+                    idx = zero_minus_1.index(".")
+                    zero_frac = zero_minus_1[idx + 1:]
+                    _part_2_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
+                zero_plus_1 = str(lines[ptr + 1]).lstrip().rstrip()
+                idx = zero_plus_1.index(".")
+                zero_frac = zero_plus_1[idx + 1:]
+                _part_1_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
+                part.append([_part_1_, _part_, _part_2_])
+                ctr = ctr + 1
+                count = count + 1
+            ptr = ptr + 1
+        parts.append(part)
+g.write(str(parts))
 g.close()
