2e3e780bc41202a80b3876d28a7896b44b6e96b4
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-24 04:50:48 +0000

    code

--- intfact.py
+++ intfact.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 
 import sys
+from zeros import zeros
+from primes import primes
 
+def further_characterize(previous_line_number,line_number):
+    f=open("./pi.txt","r")
+    g=open("./e.txt","r")
+    f.seek(previous_line_number)
+    g.seek(previous_line_number)
+    pos = previous_line_number
+    while pos < line_number:
+        c=str(f.read(1))
+        d=str(g.read(1))
+        probable_zero = int(c + d)
+        probable_prime = int(d + c)
+        if probable_zero in zeros:
+            nzeros = nzeros + 1
+        if probable_prime in primes:
+            nprimes = nprimes + 1
+        pos = pos + 1
+    f.close()
+    g.close()
+    return nzeros, nprimes
+
+    
 def characterize(rnum):
     l = len(rnum)
     f=open("./stripped_zeros.txt","r")
@@ -10,21 +33,31 @@ def characterize(rnum):
     count = 0
     ptr = 0
     nhits = 0
-    pivots = []
+    zero_pivots = []
+    prime_pivots = []
+    previous_line_number = 0
+    _nzeros_ = 0
+    _nprimes_ = 0
     while nhits < l:
         nk = int(rnum[count % l])
         line_number = line_number + nk
         _line_ = lines[line_number].lstrip().rstrip()
         _tuple_ = _line_[ptr:ptr+2]
         if _tuple_ == "00":
-            pivots.append(line_number + 1)
+            nzeros, nprimes = _further_characterize_(previous_line_number, line_number + 1)
+            _nzeros_ = _nzeros_ + nzeros
+            _nprimes_ = _nprimes_ + nprimes
+            previous_line_number = line_number + 2
+            zero_pivots.append(_nzeros_)
+            prime_pivots.append(_nprimes_)
             nhits = nhits + 1
         ptr = (ptr + 1) % 8
         count = count + 1
-    return pivots
+    return zero_pivots, prime_pivots
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
     #reverse is for analysis
-    pivots = characterize(num[::-1])
-    print(pivots)
+    zero_pivots, prime_pivots = characterize(num[::-1])
+    print(zero_pivots)
+    print(prime_pivots)
