c6e45d386b790c08577cfd13485d6244723ca780
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-24 15:06:24 +0000

    code

--- intfact.py
+++ intfact.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 from math import modf
 from zeros import zeros
 
-def characterize(rnum, limit):
+def characterize(rnum):
     l = len(rnum)
     f=open("./stripped_zeros.txt","r")
     lines = f.readlines()
@@ -16,27 +16,31 @@ def characterize(rnum, limit):
     previous_line_number = 0
     _nzeros_ = 0
     _nprimes_ = 0
-    pivots = []
     __nzeros__ = 0
-    while nhits < limit:
+    last_state = ""
+    state = 0
+    states = []
+    while nhits < l:
         nk = int(rnum[count % l])
         line_number = line_number + nk
         _line_ = lines[line_number].lstrip().rstrip()
         _tuple_ = _line_[ptr:ptr+2]
         if int(_tuple_) in zeros:
             __nzeros__ = __nzeros__ + 1
-            pivots.append(float(str(modf(Decimal(__nzeros__ / 8.0))[0])))
-            nhits = nhits + 1
+            state_description = float(str(modf(Decimal(__nzeros__ / 8.0))[0]))
+            if state == 1:
+                states.append([last_state, state_description])
+                state = 0
+                nhits = nhits + 1
+            last_state = state_description
         elif _tuple_ == "00":
-            pivots.append("00")
+            state = 1
         ptr = (ptr + 1) % 8
         count = count + 1
     f.close()
-    return pivots
+    return states
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    limit = int(sys.argv[2])
-    #reverse is for analysis
-    pivots = characterize(num[::-1], limit)
+    pivots = characterize(num[::-1])
     print(pivots)
