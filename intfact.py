6239f6bcab48c976f8ca80b92d134864bb68f1dc
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-24 03:46:08 +0000

    code

--- intfact.py
+++ intfact.py
@@ -1,77 +1,30 @@
 #!/usr/bin/python3
 
 import sys
-from decimal import Decimal
-from math import modf
-from zeros import zeros
-from zeros_parts import parts
 
-def get_fractional_part(ctr):
-    mp.prec=64
-    mp.dps=64
-    zero = str(zetazero(ctr).imag)
-    idx = zero.index(".")
-    mantissa = zero[:idx]
-    mantissa = mantissa[-2:]
-    zero = zero[idx + 1:]
-    zero = zero[:8]
-    return mantissa, zero
-
-def _get_zero_(next_pair, ctr, ptr):
-    mantissa, zero = get_fractional_part(ctr)
-    if mantissa == next_pair:
-        return True, zero[(ptr % 4)*2:(ptr % 4)*2 + 2]
-    else:
-        return False, None
-
-def get_zeros(next_pair, mid_element):
-    mp.prec=64
-    mp.dps=64
-    ctr = 1
+def characterize(rnum):
+    l = len(rnum)
+    f=open("./stripped_zeros.txt","r")
+    lines = f.readlines()
+    line_number = -1
+    count = 0
     ptr = 0
-    _set_ = []
-    while ptr < mid_element:
-        success, zero = _get_zero_(next_pair, ctr, ptr % 4)
-        if success:
-            ptr = ptr + 1
-            success = False
-            _set_.append([zero, ctr])
-        ctr = ctr + 1
-    return _set_
-
-def characterize(num):
-    l = len(num)
-    ctr = 0
-    zeros = []
-    offsets = []
-    triplets = []
-    while True:
-        triplet = ""
-        for i in range(0, 3):
-             triplet = triplet + num[(ctr + i) % l]
-        triplets.append(triplet)
-        mid_element = int(triplet[1])
-        if mid_element == 0:
-            mid_element = 10
-        next_pair = triplet[0] + triplet[2]
-        zeros.append([parts[int(triplet[0])][int(triplet[2])][:mid_element]])
-        ctr = ctr + 1
-        delta = ((l - 1) - (ctr + 1)) % 4
-        npairs = delta/2.0
-        dec = modf(Decimal(npairs))[0]
-        if dec > 0:
-            if mid_element - int(npairs) == 0:
-                npairs = [0,9]
-            else:
-                npairs = [mid_element-int(npairs), mid_element-(int(npairs) + 1)]
-        else:
-            npairs = [mid_element-int(npairs)]
-        offsets.append(npairs)
-        if ctr + 1 == l - 1:
-            break
-    return triplets,zeros,offsets
+    nhits = 0
+    pivots = []
+    while nhits < l:
+        nk = int(rnum[count % l])
+        line_number = line_number + nk
+        _line_ = lines[line_number].lstrip().rstrip()
+        _tuple_ = _line_[ptr:ptr+2]
+        if _tuple_ == "00":
+            pivots.append(line_number + 1)
+            nhits = nhits + 1
+        ptr = (ptr + 1) % 8
+        count = count + 1
+    return pivots
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    triplets,zeros, offsets = characterize(num)
-    print(list(zip(triplets,zeros, offsets)))
+    #reverse is for analysis
+    pivots = characterize(num[::-1])
+    print(pivots)
--- t.py
+++ t.py
@@ -1,46 +1,13 @@
 #!/usr/bin/python3
 import sys
-f=open("./zeros6","r")
-g=open("./zeros_parts.py","w")
+f=open("./zeros.txt","r")
+g=open("./stripped_zeros.txt","w")
 lines = f.readlines()
 f.close()
-g.write("parts=")
-part = []
-for i in range(0, 10):
-    _parts_ = []
-    for j in range(0, 10):
-        print([i, j])
-        next_match = str(i) + str(j)
-        ptr = 0
-        ctr = 0
-        count = 0
-        _part2_ = []
-        while count < 10:
-            zero = str(lines[ptr]).lstrip().rstrip()
-            idx = zero.index(".")
-            mantissa = zero[:idx]
-            mantissa = mantissa[-2:]
-            if mantissa == next_match:
-                idx = zero.index(".")
-                zero_frac = zero[idx + 1:]
-                _part_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
-                zero_minus_1 = ""
-                _part_2 = ""
-                if ptr > 0:
-                    zero_minus_1 = str(lines[ptr-1]).lstrip().rstrip()
-                    idx = zero_minus_1.index(".")
-                    zero_frac = zero_minus_1[idx + 1:]
-                    _part_2_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
-                zero_plus_1 = str(lines[ptr + 1]).lstrip().rstrip()
-                idx = zero_plus_1.index(".")
-                zero_frac = zero_plus_1[idx + 1:]
-                _part_1_ = zero_frac[(ctr % 4)*2:(ctr % 4)*2 + 2]
-                _part2_.append([_part_2_, _part_, _part_1_])
-                #input([zero[:idx], _part_2_, _part_, _part_1_])
-                ctr = ctr + 1
-                count = count + 1
-            ptr = ptr + 1
-        _parts_.append(_part2_)
-    part.append(_parts_)
-g.write(str(part))
+for line in lines:
+    l = line.rstrip().lstrip()
+    idx = l.index(".")
+    l = l[idx-1:idx + 9]
+    l = l.replace(".","")
+    g.write(str(l) + "\n")
 g.close()
