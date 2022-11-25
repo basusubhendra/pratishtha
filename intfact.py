07ec5624043572179fea537fb16a453c305870f7
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-25 12:27:28 +0530

    code

--- intfact.py
+++ intfact.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python3
 
 import sys
+from mpmath import zetazero
+from mpmath import mp
 from decimal import Decimal
 from math import modf
 from math import ceil
 from zeros100 import zeros100
 from primes100 import primes100
 from zeros import zeros
+from pi import pi
+from e import e
 
-state_encoding = [ 0, 1, 1, 0 ]
+def get_zero(ctr):
+    zero = str(zetazero(ctr).imag)
+    idx = zero.index(".")
+    zero = zero[idx-1:idx + 9]
+    return zero
 
-def further_characterize(net_hits):
+def further_characterize(net_hits, l):
+    pp = pi[:net_hits]
+    ee = e[:net_hits]
+    _ee_ = e[:net_hits][::-1]
+    ctr = 1
+    mp.prec=28
+    mp.dps=28
+    states = []
+    index = 1
+    for x in list(zip(pp, ee, _ee_)):
+        if x[1] == x[2]:
+            states.append([get_zero(index), x[0], x[1]])
+            ctr = ctr + 1 
+            if ctr == l + 1:
+                break
+        index = index + 1
+    return states
 
-def characterize(rnum, limit):
+def characterize(rnum):
     l = len(rnum)
     f=open("./stripped_zeros.dat","r")
     lines = f.readlines()
@@ -23,7 +47,7 @@ def characterize(rnum, limit):
     nhits = 0
     __nzeros__ = 0
     net_hits = 0
-    while nhits < limit:
+    while True:
         nk = int(rnum[count % l])
         line_number = line_number + nk
         _line_ = lines[line_number].lstrip().rstrip()
@@ -31,21 +55,18 @@ def characterize(rnum, limit):
         if int(_tuple_) in zeros:
             net_hits = net_hits + 1
             __nzeros__ = __nzeros__ + 1
-            state_description = str(modf(Decimal(__nzeros__ / 8.0))[0])
-            states.append(encode(state_description))
         elif _tuple_ == "00":
             net_hits = net_hits + 1
             if net_hits in zeros:
-                z_contrib, p_contrib = further_characterize(net_hits)
-            states.append("**")
+                states = further_characterize(net_hits, l)
+                break
             nhits = nhits + 1
         ptr = (ptr + 1) % 8
         count = count + 1
     f.close()
-    return states
+    return states 
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    limit = int(sys.argv[2])
-    pivots = characterize(num, limit)
+    pivots = characterize(num)
     print(pivots)
