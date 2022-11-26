98df966775fc9b91b02affade949e284034a11ef
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-26 12:27:04 +0530

    code

--- intfact.py
+++ intfact.py
@@ -38,10 +38,10 @@ def characterize(rnum):
     line_number = -1
     count = 0
     ptr = 0
-    p_states = [] 
-    e_states = [] 
+    states = [] 
     nhits = 0
     net_hits = 0
+    niter = 2
     while True:
         nk = int(rnum[count % l])
         line_number = line_number + nk
@@ -53,19 +53,18 @@ def characterize(rnum):
             net_hits = net_hits + 1
             if net_hits in zeros:
                 state_description = further_characterize(net_hits)
-                p_states.append(state_description)
-                e_states.append(state_description[::-1])
+                states.append(state_description)
                 nhits = nhits + 1
-                if nhits == l:
+                if nhits == niter*l:
                     break
         ptr = (ptr + 1) % 8
         count = count + 1
     f.close()
-    return p_states , e_states
+    return states
 
 def _count_(x, y):
-    print(y, x)
     pp = ""
+    t = 0
     if y == 0:
         pp = pi
     else:
@@ -81,38 +80,51 @@ def _count_(x, y):
             _ctr_ = ctr + 1
             _ctr2_ = ctr2 + 1
             _snippet_ = "1"
-            while pp[_ctr_] == x[_ctr2_] and pp[_ctr_] == "0":
+            while _ctr2_ < l and pp[_ctr_] == x[_ctr2_] and pp[_ctr_] == "0":
                 _snippet_ = _snippet_ + "1"
                 _ctr_ = _ctr_ + 1
                 _ctr2_ = _ctr2_ + 1
             count = count + int(_snippet_, 2)
             ctr2 = _ctr2_
             ctr = 0
+            t = 1 - t
+            if t == 0 and y == 0:
+                pp = pi
+            elif t == 0 and y == 1:
+                pp = e
+            elif t == 1 and y == 0:
+                pp = e
+            elif t == 1 and y == 1:
+                pp = pi
         else:
             ctr = ctr + 1
             ctr2 = ctr2 + 1
     _snippet_ = str(bin(count)[2:])
-    if y == 0:
-        return _snippet_[::-1]
-    elif y == 1:
-        return _snippet_
+    return _snippet_
 
 def factorize(states, p, q):
     factor = ""
+    t = 0
     for state in states:
         l = len(state)
         factor_snippet = _count_(state, p)
-        print(p, factor_snippet)
+        if t == 0 and p == 1:
+           print(p, factor_snippet)
+        elif t == 1 and p == 1:
+           print(p, factor_snippet[::-1])
+        elif p == 0:
+           print(p, factor_snippet)
         factor = factor + factor_snippet
+        t = 1 - t
     q.put(int(factor,2))
     return
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
     q = Queue()
-    p_states, e_states = characterize(num)
-    p1 = Thread(target = factorize, args = (p_states, 0, q, ))
-    p2 = Thread(target = factorize, args = (e_states, 1, q, ))
+    states = characterize(num)
+    p1 = Thread(target = factorize, args = (states, 0, q, ))
+    p2 = Thread(target = factorize, args = (states, 1, q, ))
     p1.start()
     p2.start()
     p1.join()
