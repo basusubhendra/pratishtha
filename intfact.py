c68b923d998d7c110479f39a6106da2f2be96deb
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-27 12:03:45 +0530

    code

--- intfact.py
+++ intfact.py
@@ -3,6 +3,7 @@
 import sys
 from mpmath import zetazero
 from mpmath import mp
+from gmpy2 import *
 from zeros import zeros
 from pi import pi
 from e import e
@@ -10,10 +11,11 @@ from e import e
 def get_zero(ctr):
     zero = str(zetazero(ctr).imag)
     idx = zero.index(".")
-    zero = zero[idx-1:idx + 9]
-    return zero
+    frac = zero[idx:idx + 9]
+    mantissa = zero[:idx]
+    return mantissa, frac
 
-def further_characterize(net_hits):
+def characterize(net_hits):
     pp = pi[:net_hits]
     ee = e[:net_hits]
     _ee_ = e[:net_hits][::-1]
@@ -23,12 +25,15 @@ def further_characterize(net_hits):
     index = 1
     for x in list(zip(pp, ee, _ee_)):
         if x[1] == x[2]:
-            zero = str(get_zero(index))
-            states.append(zero[1:])
+            mantissa, frac = get_zero(index)
+            states.append([int(mantissa), frac])
         index = index + 1
     return states
 
-def characterize(rnum):
+def interpret(state):
+    pass
+
+def factorize(rnum):
     l = len(rnum)
     f=open("./stripped_zeros.dat","r")
     lines = f.readlines()
@@ -36,8 +41,8 @@ def characterize(rnum):
     count = 0
     ptr = 0
     net_hits = 0
-    nhits = 0
-    states = []
+    prod = gmpy2.mpz("1")
+    factors = []
     while True:
         nk = int(rnum[count % l])
         line_number = line_number + nk
@@ -47,47 +52,19 @@ def characterize(rnum):
             net_hits = net_hits + 1
         elif _tuple_ == "00":
             if net_hits in zeros:
-                state_description = further_characterize(net_hits)
-                states.append(state_description)
-                nhits = nhits + 1
-                if nhits == l:
-                    break
+                state_description = characterize(net_hits)
+                input(state_description)
+                factor = interpret(state_description)
+                #factors.append(int(factor[::-1], 2))
+                #prod = gmpy2.mul(prod, gmpy2.mpz(str(factor)))
+                #if prod == gmpy2.mpz(num):
+                #    break
         ptr = (ptr + 1) % 8
         count = count + 1
     f.close()
     return states
 
-def _count_(x, y):
-    pp = ""
-    if y == 0:
-        pp = pi
-    else:
-        pp = e
-    ctr = 0
-    ctr2 = 0
-    l = len(x)
-    count = 0
-    while ctr2 < l:
-        ss = x[ctr2]
-        _pk_ = pp[ctr]
-        if _pk_ == ss and ss == "0":
-            _ctr_ = ctr + 1
-            _ctr2_ = ctr2 + 1
-            _snippet_ = "1"
-            while _ctr2_ < l and pp[_ctr_] == x[_ctr2_] and pp[_ctr_] == "0":
-                _snippet_ = _snippet_ + "1"
-                _ctr_ = _ctr_ + 1
-                _ctr2_ = _ctr2_ + 1
-            count = count + int(_snippet_, 2)
-            ctr2 = _ctr2_
-            ctr = 0
-        else:
-            ctr = ctr + 1
-            ctr2 = ctr2 + 1
-    _snippet_ = str(bin(count)[2:])
-    return _snippet_
-
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    states = characterize(num)
-    print(states)
+    factors = factorize(num)
+    print(factors)
