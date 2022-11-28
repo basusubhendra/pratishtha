1855d3bf4a4b286ecbca35fb4655587d408b900b
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-28 05:39:28 +0530

    code

--- intfact.py
+++ intfact.py
@@ -2,7 +2,6 @@
 
 import sys
 from gmpy2 import *
-from stripped_zeros import stripped_zeros
 
 def prod(f1, f2):
     _prod_ = gmpy2.mul(gmpy2.mpz(f1), gmpy2.mpz(f2))
@@ -25,7 +24,7 @@ def factorize(rnum):
         _line_ = lines[line_number].lstrip().rstrip()
         _tuple_ = _line_[ptr:ptr+2]
         if _tuple_ == "00":
-            state_description = stripped_zeros[line_number]
+            state_description = _line_
             input(state_description)
             print("!!==================!!")
             #if prod(factor1, factor2) == gmpy2.mpz(num):
