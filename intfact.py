6afa41b98d31a392c6199c5991b186027a946caa
Subhendra Basu <basu.subhendra@outlook.com>
2022-11-28 08:10:49 +0530

    code

--- intfact.py
+++ intfact.py
@@ -2,10 +2,34 @@
 
 import sys
 from gmpy2 import *
+from pi import pi
+from e import e
+from threading import Thread
+from queue import Queue
 
 def prod(f1, f2):
     _prod_ = gmpy2.mul(gmpy2.mpz(f1), gmpy2.mpz(f2))
     return str(_prod_)
+
+def _match_(line, pp, param, q):
+    succ = 1
+    for x in pp:
+        if not x in pp:
+            succ = None
+            break
+    q.put([param, succ])
+    return
+
+def divisibleBy(num, factor):
+    nz = gmpy2.mpz(num)
+    fz = gmpy2.mpz(factor)
+    if fz <= gmpy2.mpz("1"):
+        return False
+    modz = gmpy2.f_mod(nz, fz)
+    if modz == gmpy2.mpz("0"):
+        return True
+    else:
+        return False
 
 def factorize(rnum):
     l = len(rnum)
@@ -18,14 +42,54 @@ def factorize(rnum):
     prod = gmpy2.mpz("1")
     factor1 = []
     factor2 = []
+    nmatches1 = 0
+    namtches2 = 0
+    ctr = 0
+    factors = []
+    factor = ""
+    offset = 0
     while True:
         nk = int(rnum[count % l])
         line_number = line_number + nk
         _line_ = lines[line_number].lstrip().rstrip()
         _tuple_ = _line_[ptr:ptr+2]
         if _tuple_ == "00":
-            state_description = _line_
-            input(state_description)
+            q = Queue()
+            t1 = Thread(target=_match_, args=(_line_, pi[ctr:ctr + 5], 0, q,  ))
+            t2 = Thread(target=_match_, args=(_line_, e[ctr:ctr + 5], 1, q,  ))
+            t1.start()
+            t2.start()
+            t1.join()
+            t2.join()
+            c = []
+            while not q.empty():
+                c.append(q.get())
+            input(c)
+            if c[0][1] != None and c[1][1] != None:
+                ctr = 0
+                synth_vector = synth_vector + str(bin(nmatches1)[2:])
+                factor = factor + str(bin(nmatches2[::-1])[2:])
+                dec_factor = int(factor, 2)
+                if divisibleBy(rnum, dec_factor) == True:
+                    factors.append(dec_factor)
+                    factor = ""
+                if synth_vector in str(bin(rnum)[2:])[::-1][offset:]:
+                    index = str(bin(rnum)[2:])[::-1][offset:].index(synth_vector)
+                    if index == 0:
+                        offset = offset + len(synth_vector)
+                        if offset == l:
+                            break
+                else:
+                    print(rnum + " is a prime number.")
+                    sys.exit(0)
+            elif c[0][1] != None:
+                nmatches1 = nmatches1 + 1
+                ctr = ctr + 3
+            elif c[1][1] != None:
+                nmatches2 = nmatches2 + 1
+                ctr = ctr + 3
+            else:
+                ctr = ctr + 3
             print("!!==================!!")
             #if prod(factor1, factor2) == gmpy2.mpz(num):
             #    break
