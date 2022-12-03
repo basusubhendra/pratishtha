e20b9924b622b5e522d37491196dc096b83f7d72
Subhendra Basu <basu.subhendra@outlook.com>
2022-12-03 19:32:25 +0530

    code

--- intfact.py
+++ intfact.py
@@ -14,12 +14,12 @@ def characterize(num, precision):
             triplet = triplet + num[(ctr + i) % l]
         score = triplet[0] + triplet[2] + "." + triplet[1]
         if score == "00.0":
-            triplets.append("00.0")
+            triplets.append([triplet[2],"00.0"])
         else:
             log_score = str(mpmath.log(score))
             index = log_score.index(".")
             log_score = log_score[index + 1:]
-            triplets.append(log_score)
+            triplets.append([triplet[2],log_score])
         ctr = ctr + 1
         if (ctr + 3) > l:
             break
@@ -30,3 +30,4 @@ if __name__ == "__main__":
     precision = int(sys.argv[2])
     triplets = characterize(num, precision)
     print(triplets)
+
