151a1927cbb79d04f7450507df798f3d128da6ac
Subhendra Basu <basu.subhendra@outlook.com>
2022-12-03 17:38:06 +0530

    code

--- intfact.py
+++ intfact.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 import sys
+import mpmath
 
-def characterize(num):
+def characterize(num, precision):
     ctr = 0
     l = len(num)
+    mpmath.mp.dps = precision
+    mpmath.mp.prec = precision
     triplets = []
     while True:
         triplet = ""
         for i in range(0, 3):
             triplet = triplet + num[(ctr + i) % l]
-        triplets.append(triplet)
+        score = triplet[0] + triplet[2] + "." + triplet[1]
+        if score == 0.0:
+            triplets.append(str(mpmath.exp(1)))
+        else:
+            triplets.append(str(mpmath.log(score)))
         ctr = ctr + 1
         if (ctr + 3) > l:
             break
@@ -17,5 +24,6 @@ def characterize(num):
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
-    triplets = characterize(num)
+    precision = int(sys.argv[2])
+    triplets = characterize(num, precision)
     print(triplets)
